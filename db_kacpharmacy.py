fad7abf1e7d4ea98a997391cee40a72f5bf5ee49
Katherine Simmons <113742922+Kaysimmons97@users.noreply.github.com>
2022-11-29 18:00:01 -0500

    Add files via upload

--- db_kacpharmacy.py
+++ db_kacpharmacy.py
@@ -3,7 +3,7 @@
 # Connect to database
 db = DAL('sqlite://storage.sqlite')
 
-# This creates the Patients Table
+# This creates the Contacts Table
 db.define_table(
    'Patients',
    #Field('Contact ID', notnull = True, unique = True),
@@ -28,4 +28,50 @@ db.define_table(
    Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
    Field('Webpage'),
    Field('Email', requires = IS_EMAIL())
+)
+
+#contacts table should be renamed to 'Patients'
+    #table needs to include 'Policy Number' , 'Bin Number', 'Cardholder Name' , 'DOB', 'Alergies'
+db.define_table(
+   'Prescribers',
+   Field('prescriber ID', notnull = True, unique = True),
+   Field('First Name', notnull = True 'reference company'),
+   Field('Last Name', notnull = True ),
+   Field('DEA number', unique = True),
+   Field('NPI number', notnull = True, unique = True),
+   Field('Address' notnull = True),
+   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+'))
+   Filed('Fax Number', requires = IS_MATCH('[\d\-\(\) ]+'))
+   format = '%(name)s'
+)
+
+db.define_table(
+   'Perscriptions',
+   Field('Rx Number', notnull = True) unique = True), format = '%(name)s'),
+   Field('Brand Name', notnull = True),
+`  Field('Generic Name', notnull = True),
+   Field('DAW', notnull = True),
+   Field('NDC Number', notnull = True),
+   Field('Quanty', notnull = True),
+   Field('SIG', notnull = True),
+   Field('Prescriber', notnull = True),
+`  Field('Refills', notnull = True),
+   Field('Drug Schedule')
+
+
+db.define_table(
+   'Drug Schedules',
+   Field('ID', notnull = True, unique = True),
+   Field('Classification Name' notnull = True),
+   format = '%(name)s'
+)
+
+db.define_table(
+    'Allergy Profiles'
+    Field('Status')
+    Field('Entry Dates')
+    Field('Severity')
+    Field('Raction')
+    
+
 )
