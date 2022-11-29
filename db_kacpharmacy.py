1051da4dfd207396cc816800f8de7e02d16dd3c6
Alec Stott <Alecsttt9@gmail.com>
2022-11-29 09:38:35 -0500

    Update db_kacpharmacy.py

--- db_kacpharmacy.py
+++ db_kacpharmacy.py
@@ -1,29 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# This creates the Contacts Table
 db.define_table(
    'Contacts',
-   Field('Contact ID', notnull = True),
-   Field('company', 'reference company'),
-   Field('picture', 'upload'),
-   Field('email', requires = IS_EMAIL()),
-   Field('phone_number', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('address'),
-   format = '%(name)s'
+   Field('Contact ID', notnull = True, unique = True),
+   Field('First Name'),
+   Field('Last Name'),
+   Field('Email', requires = IS_EMAIL()),
+   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('Address One'),
+   Field('Address Two'),
+   Field('Prescriber', 'reference Prescriber id'), # db.Prescribers
+   Field('Insurer', 'reference Insurer id'), # db.Insurance
+   Field('Prescriptions'),
+   format = '%(name)s'  # what is this?
 )
 
+# This creates the Insurance Table
 db.define_table(
-   'company',
-   Field('name', notnull = True, unique = True), format = '%(name)s')
-
-
-
-db.define_table(
-   'Contacts',
-   Field('Contact ID', notnull = True),
-   Field('company', 'reference company'),
-   Field('picture', 'upload'),
-   Field('email', requires = IS_EMAIL()),
-   Field('phone_number', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('address'),
-   format = '%(name)s'
+   'Insurance',
+   Field('Insurer id', notnull = True, unique = True), 
+   Field('Insurer Name', notnull = True),
+   Field('Address'),
+   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('Webpage'),
+   Field('Email', requires = IS_EMAIL())
 )
