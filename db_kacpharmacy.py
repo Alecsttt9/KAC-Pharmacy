d54163c536757ca1e65c8701122a291255572aaf
Alec Stott <Alecsttt9@gmail.com>
2022-11-29 18:49:50 -0500

    Update db_kacpharmacy.py

--- db_kacpharmacy.py
+++ db_kacpharmacy.py
@@ -1,57 +1,52 @@
 # -*- coding: utf-8 -*-
 
-# Connect to database
-db = DAL('sqlite://storage.sqlite')
-
 # This creates the Contacts Table
 db.define_table(
    'Prescribers',
    #Field('Prescriber ID', notnull = True, unique = True),
-   Field('First Name', notnull = True 'reference company'),
-   Field('Last Name', notnull = True ),
-   Field('DEA number', unique = True, requires = IS_MATCH('[\d\-\(\) ]+'),
-   Field('NPI number', notnull = True, unique = True, requires = IS_MATCH('[\d\-\(\) ]+'),
+   Field('firstName', 'reference company', notnull = True),
+   Field('lastName', notnull = True ),
+   Field('deaNumber', unique = True, requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('npiNumber', notnull = True, unique = True, requires = IS_MATCH('[\d\-\(\) ]+')),
    Field('Address' notnull = True),
-   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+'))
-   Filed('Fax Number', requires = IS_MATCH('[\d\-\(\) ]+'))
-   format = '%(name)s'
-
-   db.define_table(
-   'Insurances',
-   #Field('Insurer id', notnull = True, unique = True), 
-   Field('Company Name', notnull = True),
-   Field('Policy Number', notnull = True),
-   Field ('Bin Number', requires = IS_MATCH('[\d\-\(\) ]+')), 
-   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('Primary Cardholder'),
-   )
-   
-   db.define_table(
-    'Allergy Profiles'
-    Field('Status', notnull = True)
-    Field('Medication Name')
-    Field('Entry Date',type='datetime', default=datetime.datetime.now(), notnull = True)
-    Field('Severity')
-    Field('Raction')
-   )
+   Field('phoneNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Filed('faxNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
+   format = '%(name)s')
 
 db.define_table(
-   'Perscriptions',
-   Field('Rx Number', notnull = True unique = True, format = '%(name)s'),
-   Field('Brand Name', notnull = True),
-   Field('Generic Name', notnull = True),
+   'Insurances',
+   #Field('Insurer id', notnull = True, unique = True), 
+   Field('companyName', notnull = True),
+   Field('policyNumber', notnull = True),
+   Field ('binNumber', requires = IS_MATCH('[\d\-\(\) ]+')), 
+   Field('phoneNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('primaryCardholder'))
+   
+db.define_table(
+    'Allergy Profiles',
+    Field('Status', notnull = True),
+    Field('medicationName'),
+    Field('entryDate',type='datetime', default=datetime.datetime.now(), notnull = True),
+    Field('Severity'),
+    Field('Reaction'))
+
+db.define_table(
+   'Prescriptions',
+   Field('rxNumber', notnull = True, unique = True, format = '%(name)s'),
+   Field('brandName', notnull = True),
+   Field('genericName', notnull = True),
    Field('DAW', notnull = True),
-   Field('NDC Number', notnull = True),
-   Field('Quanty', notnull = True),
+   Field('ndcNumber', notnull = True),
+   Field('Quantity', notnull = True),
    Field('SIG', notnull = True),
    Field('Prescriber', notnull = True),
    Field('Refills', notnull = True),
-   Field('Drug Schedule', 'refrence Schedule ID')
+   Field('drugSchedule', 'refrence Schedule ID')
 )
 db.define_table(
    'Drug Schedules',
    #Field('ID', notnull = True, unique = True),
-   Field('Schedule' notnull = True),
+   Field('Schedule', notnull = True),
    Field('Classification', notnull = True)
    format = '%(name)s'
 )
@@ -59,17 +54,17 @@ db.define_table(
 db.define_table(
    'Patients',
    #Field('Contact ID', notnull = True, unique = True),
-   Field('First Name'),
-   Field('Last Name'),
+   Field('firstName'),
+   Field('lastName'),
    Field('DOB')
-   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('Address 1', 'refrence States'),
-   Field('Address 2'),
+   Field('phoneNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('Address1', 'refrence States'),
+   Field('Address2'),
    Field('Prescriber', 'reference Prescribers'), # db.Prescribers
    Field('Insurer', 'reference Insurances'), # db.Insurance
    Field('Allergies', 'refrence Allergy Profiles')
    Field('Prescriptions', 'refrence Perscriptions'),
-   format = '%(name)s'  # what is this?
+   format = '%(name)s'  
 )
 
 # This creates the Insurance Table
