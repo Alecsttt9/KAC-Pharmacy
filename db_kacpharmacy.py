181ec753378b3adbd6e07d863169ae34117825b1
Katherine Simmons <113742922+Kaysimmons97@users.noreply.github.com>
2022-12-01 18:31:47 -0500

    Add files via upload

--- db_kacpharmacy.py
+++ db_kacpharmacy.py
@@ -1,84 +1,88 @@
 # -*- coding: utf-8 -*-
 
-import datetime
-#test comment
-db.define_table(
-    'States',
-    Field('name', notnull = True, unique = True),
-    Field('abbreviation', notnull = True, unique = True),
-    format = '%(name)s')
+# Connect to database
+db = DAL('sqlite://storage.sqlite')
 
 # This creates the Contacts Table
 db.define_table(
    'Prescribers',
-   Field('firstName',  notnull = True),
-   Field('lastName', notnull = True ),
-   Field('deaNumber', unique = True, requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('npiNumber', notnull = True, unique = True, requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('Address', notnull = True),
-   Field('phoneNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('faxNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
-   format = '%(lastName)s')
-
-db.define_table(
+   #Field('Prescriber ID', notnull = True, unique = True),
+   Field('First Name', notnull = True),
+   Field('Last Name', notnull = True),
+   Field('DEA number', unique = True, requires = IS_MATCH('[\d\-\(\) ]+'),
+   Field('NPI number', notnull = True, unique = True, requires = IS_MATCH('[\d\-\(\) ]+'),
+   Field('Address' notnull = True),
+   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+'))
+   Filed('Fax Number', requires = IS_MATCH('[\d\-\(\) ]+'))
+   format = '%(name)s'
+   )
+   db.define_table(
    'Insurances',
-   Field('companyName', notnull = True),
-   Field('policyNumber', notnull = True),
-   Field ('binNumber', requires = IS_MATCH('[\d\-\(\) ]+')), 
-   Field('phoneNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('primaryCardholder'))
-   
-db.define_table(
-    'allergyProfiles',
-    Field('Status', notnull = True),
-    Field('medicationName'),
-    Field('entryDate', type='datetime', default=datetime.datetime.now(), notnull = True),
-    Field('Severity'),
-    Field('Reaction'))
+   #Field('Insurer id', notnull = True, unique = True), 
+   Field('Company Name', notnull = True),
+   Field('Policy Number', notnull = True),
+   Field ('Bin Number', requires = IS_MATCH('[\d\-\(\) ]+')), 
+   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('Primary Cardholder', requires = IS_MATCH('[\d\-\(\) ]+')),
+   )
+   db.define_table(
+    'Allergy Profiles'
+    Field('Status', notnull = True)
+    Field('Medication Name')
+    Field('Entry Date',type='datetime', default=datetime.datetime.now(), notnull = True)
+    Field('Severity')
+    Field('Raction')
 
 db.define_table(
-   'drugSchedules',
-   #Field('ID', notnull = True, unique = True),
-   Field('Schedule', notnull = True),
-   Field('Classification', notnull = True),
-   format = '%(classification)s'
-)
-
-db.define_table(
-   'Prescriptions',
-   Field('rxNumber', notnull = True, unique = True, format = '%(name)s'),
-   Field('brandName', notnull = True),
-   Field('genericName', notnull = True),
+   'Perscriptions',
+   Field('Rx Number', notnull = True) unique = True), format = '%(name)s'),
+   Field('Brand Name', notnull = True),
+`  Field('Generic Name', notnull = True),
    Field('DAW', notnull = True),
-   Field('ndcNumber', notnull = True),
-   Field('Quantity', notnull = True),
+   Field('NDC Number', notnull = True),
+   Field('Quanty', notnull = True),
    Field('SIG', notnull = True),
    Field('Prescriber', notnull = True),
-   Field('Refills', notnull = True),
-   Field('drugSchedule', 'reference drugSchedules')
-)
+`  Field('Refills', notnull = True),
+   Field('Drug Schedule', 'refrence Drug Schedules')
 
 db.define_table(
-    'leadSource',
-    Field('leadSource', notnull = True, unique = True),
-    format = '%(leadSource)s')
+   'Drug Schedules',
+   #Field('ID', notnull = True, unique = True),
+   Field('Schedule' notnull = True),
+   Field('Classification', notnull = True)
+   format = '%(name)s'
+)
 
 db.define_table(
    'Patients',
    #Field('Contact ID', notnull = True, unique = True),
-   Field('firstName'),
-   Field('lastName'),
-   Field('DOB'),
-   Field('phoneNumber', requires = IS_MATCH('[\d\-\(\) ]+')),
-   Field('Address1'),
-   Field('Address2'),
-   Field('City'),
-   Field('stateName', 'reference States'),
-   Field('zipCode'),
+   Field('First Name'),
+   Field('Last Name'),
+   Field('DOB')
+   Field('Phone Number', requires = IS_MATCH('[\d\-\(\) ]+')),
+   Field('Address 1', 'refrence States'),
+   Field('Address 2'),
    Field('Prescriber', 'reference Prescribers'), # db.Prescribers
    Field('Insurer', 'reference Insurances'), # db.Insurance
-   Field('Allergies', 'reference allergyProfiles'),
-   Field('Prescriptions', 'reference Prescriptions'),
-   Field('leadSource', 'reference leadSource'),
-   format = '%(lastName)s'  
+   Field('Allergies', 'refrence Allergy Profiles')
+   Field('Prescriptions', 'refrence Perscriptions'),
+   format = '%(name)s'  # what is this?
 )
+
+# This creates the Insurance Table
+
+   
+)
+
+#contacts table should be renamed to 'Patients'
+    #table needs to include 'Policy Number' , 'Bin Number', 'Cardholder Name' , 'DOB', 'Alergies'
+
+)
+
+
+
+
+
+
+
